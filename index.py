5751bc16a4207fbcf98bef1a6df70cf8e52d093d
piroQ <fuyofuyopiroq@gmail.com>
2023-01-24 20:40:27 +0900

    password1

---
+++ index.py
@@ -0,0 +1,7 @@
+import random
+import string
+def randomname(n):
+    ranpass=[random.choice(string.ascii_letters + string.digits) for i in range(n)]
+    result="".join(ranpass)
+    print(result)
+randomname(12)
